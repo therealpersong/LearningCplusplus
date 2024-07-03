85dd3184e8a511192992ee44a2d2da661d01ff77
therealpersong <timotheussoares5@gmail.com>
2024-07-03 11:34:17 +0100

    test commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+#include <istream>
+#include <string>
 
-   std::cin >> num1;
+
+using std::cout;
+using std::string;
+using std::cin;
+
+
+int main()
+{
+
+string name;
+
+while(name.empty())
+{
+    cout << "Enter your name" << std::endl;
+    std::getline(cin, name);
+}
+
+cout << "The name is " << name << '\n';
+    return 0;
+}
