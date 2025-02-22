0184a3a26c13b11b892767fbd6fef99811d852bc
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-02-22 21:16:30 +0000

    Update main.cpp
    user input

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
-int main(){
+// cout << (insertion operator)
+// cin >> (extraction operator)
 
-int correct = 8;
-int questions = 10;
-double score = correct/(double)questions * 100;
+int main() 
+{
+   std::string name;
+   int age;
 
-std::cout << score << "%";
+   std::cout << "What's your full name?: ";
+   std::getline(std::cin >> std::ws, name);
 
+   std::cout << "What's your age?: ";
+   std::cin >> age;
+
+   std::cout << "Hello " << name << '\n';
+   std::cout << "You are " << age << " years old";
+
+   return 0;
 }
