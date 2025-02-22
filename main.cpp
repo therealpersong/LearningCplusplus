43c2fc3e1b1f94f365ab3ae5824a05b3cc0ee143
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-02-22 21:30:21 +0000

    Update main.cpp
    math functions

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
-
-// cout << (insertion operator)
-// cin >> (extraction operator)
+#include <cmath>
 
 int main() 
 {
-   std::string name;
-   int age;
+   double x = 3.99;
+   double y = 4;
+   double z;
 
-   std::cout << "What's your full name?: ";
-   std::getline(std::cin >> std::ws, name);
+   //z = std::max(x, y);
+   //z = std::min(x, y);
+   //z = pow(2, 4);
+   //z = sqrt(9);
+   //z = abs(-3);
+   //z = round(x);
+   //z = ceil(x);
+   //z = floor(x);
 
-   std::cout << "What's your age?: ";
-   std::cin >> age;
-
-   std::cout << "Hello " << name << '\n';
-   std::cout << "You are " << age << " years old";
+   std::cout << z;
 
    return 0;
 }
+
+// useful math functions ref link: https://cplusplus.com/reference/cmath/
